dba76badc909ce90b47fd1b672d0bde890c0a9c6
Javier <morenojavier@gmail.com>
2016-06-09 16:44:53 -0500

    Prueba de cambio fuerte

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Home</title>
+</head>
+<body>
+
+	<header>Header</header>
+	<section>Contenido</section>
+	<footer>Footer</footer>
+	<div class="copyright">Copyright</div>
+</body>
+</html>
